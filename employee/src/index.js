d17ab8cf553db246004545ae553cf035d20e23f9
The Noise <leespaulding25@gmail.com>
2021-05-05 15:29:25 -0400

    deleting stuff to get my work toshow when i do npm start

--- employee/src/index.js
+++ employee/src/index.js
@@ -4,10 +4,6 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <App />, document.getElementById('root'));
 
 
