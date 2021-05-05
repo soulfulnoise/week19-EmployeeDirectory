fe54b80c46708c62bac3dc7b92d2048c11c2ba7f
The Noise <leespaulding25@gmail.com>
2021-05-05 11:27:49 -0400

    imported componet and api to container

---
+++ employee/src/componets/Container/EmployeeContainer.jsx
@@ -0,0 +1,5 @@
+//importing components and Api
+import React, { Component } from "react";
+import SearchBar from "../SearchBar";
+import EmployeeTable from "../Employee Table";
+import API from"../../utils/API";
