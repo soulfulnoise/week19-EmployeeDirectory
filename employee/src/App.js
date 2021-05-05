b271ee153b3ebebb8e120378c9c2f4ca6266069b
The Noise <leespaulding25@gmail.com>
2021-05-05 15:22:50 -0400

    added header and my container

--- employee/src/App.js
+++ employee/src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import Header from "./componets/Header";
 import EmployeeContainer from "./componets/Container/EmployeeContainer";
 
-
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <EmployeeContainer/>
+    </div>
+  )
+}
 
 export default App;
