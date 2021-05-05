7843e432a1ade84acdf091fc5eb0cdd13e88ad15
The Noise <leespaulding25@gmail.com>
2021-05-05 17:17:50 -0400

    changed the number of user to pull in from the api

--- employee/src/utils/API.js
+++ employee/src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// pulling in api for 5000 employees and us based work group
-const URL ="https://randomuser.me/api/?results=5000&nat=us"
+// pulling in api for 500 employees and us based work group
+const URL ="https://randomuser.me/api/?results=300&nat=us"
 
 export default {getEmployees: function() {return axios.get(URL)}};
